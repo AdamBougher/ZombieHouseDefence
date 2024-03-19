834973bdca18a25efa168a25e6d9946a71a4ecc2
AdamBougher <a.bougher1999@gmail.com>
2024-03-18 19:15:24 -0500

    updated logic

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using NavMeshPlus.Components;
 using UnityEngine;
 
@@ -37,13 +39,20 @@ public class Door : MonoBehaviour, IHittable
     public void Damage(int amt)
     {
         hp -= amt;
+        
         if (hp > 0) return;
         
         GetComponent<BoxCollider2D>().enabled = false;
+        foreach (var boxCollider2D in gameObject.GetComponents<BoxCollider2D>())
+        {
+            boxCollider2D.enabled = false;
+        }
         
         _surface2D.UpdateNavMesh(_surface2D.navMeshData);
-        
+
+
         Destroy(gameObject);
+        
     }
 
     public void Fix(int amt)
