f84f60bfeaaa4d4472a985609c28fa20461a7110
AdamBougher <a.bougher1999@gmail.com>
2024-03-18 19:08:44 -0500

    updated DOTween

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,19 +2,41 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.Tilemaps;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
     private Rigidbody2D _rb;
-
     private int _damageAmt;
+    
+    private Vector2 _pausedVelocity;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        GameManager.Pause += OnPaused;
+        GameManager.Unpause += OnResume;
+        
         //StartCoroutine(Decay());
+    }
+    
+    private void OnDestroy()
+    {
+        GameManager.Pause -= OnPaused;
+        GameManager.Unpause -= OnResume;
+    }
+    
+    private void OnPaused()
+    {
+        _pausedVelocity = _rb.velocity;
+        _rb.velocity = Vector2.zero;
+    }
+    
+    private void OnResume()
+    {
+        _rb.velocity = _pausedVelocity;
     }
 
     public void StartBullet(Vector2 direction, int speed,int damage)
