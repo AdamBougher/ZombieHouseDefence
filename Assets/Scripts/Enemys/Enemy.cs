789e016d7287ccecf4fda04e98edf2e18e94ea7a
AdamBougher <a.bougher1999@gmail.com>
2024-09-15 09:41:45 -0500

    fixed spelling

--- Assets/Scripts/Enemys/Enemy.cs
+++ Assets/Scripts/Enemys/Enemy.cs
@@ -45,7 +45,7 @@ public class Enemy : Character
         _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         _collider = GetComponent<CircleCollider2D>();
 
-        //setup instances varbales
+        //setup instances variables
         _agent.updateRotation = false;
         _agent.updateUpAxis = false;
         _agent.speed = speed;
@@ -163,7 +163,7 @@ public class Enemy : Character
         //waite while ending shit is happening
         yield return new WaitWhile(() => AudioSource.isPlaying);
         
-        EnemyPool.SharedInstance.ReturnToPool(this.gameObject);
+        EnemyPool.SharedInstance.ReturnToPool(this);
     }
 
     private void LevelUp(object sender, EventArgs e)
