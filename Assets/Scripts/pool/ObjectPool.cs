c37989465c373730a1f9bf36eecb3295d9ae21c2
AdamBougher <a.bougher1999@gmail.com>
2024-09-15 09:41:56 -0500

    updated to be what i wanbted it to be

--- Assets/Scripts/pool/ObjectPool.cs
+++ Assets/Scripts/pool/ObjectPool.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public abstract class ObjectPool<T> : MonoBehaviour
+public abstract class ObjectPool<T> : MonoBehaviour where T : Component
 {
-    public static ObjectPool<T> SharedInstance;
-    public List<GameObject> pooledObjects;
-    public GameObject objectToPool;
-    public Transform poolContainer;
-    [FormerlySerializedAs("Amount")] public int amount;
-
+    private static ObjectPool<T> _sharedInstance;
+    public static ObjectPool<T> SharedInstance
+    {
+        get
+        {
+            if (_sharedInstance == null)
+            {
+                _sharedInstance = FindObjectOfType<ObjectPool<T>>();
+                if (_sharedInstance == null)
+                {
+                    Debug.LogError("No instance of ObjectPool found.");
+                }
+            }
+            return _sharedInstance;
+        }
+    }
+    
+    public T objectToPool;
+    
+    public int amount;
+    
+    public List<T> pooledObjects;
+    
+    
+    private Transform PoolContainer => gameObject.transform;
+    
     protected int AmountToPool
     {
         get {
@@ -23,38 +45,57 @@ public abstract class ObjectPool<T> : MonoBehaviour
         set => amount = value;
     }
 
+    private int _count = 0;
+    
     private void Awake()
     {
-        SharedInstance = this;
+        if (_sharedInstance != null && _sharedInstance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _sharedInstance = this;
+        }
     }
 
     private void Start()
     {
-        pooledObjects = new List<GameObject>();
-        GameObject tmp;
-        for (int i = 0; i < AmountToPool; i++)
+        pooledObjects = new();
+        for (var i = 0; i < AmountToPool; i++)
         {
-            tmp = Instantiate(objectToPool);
-            tmp.SetActive(false);
-            pooledObjects.Add(tmp);
-            tmp.transform.parent = poolContainer.transform;
+            AddObjectToPool();
         }
     }
 
-    public GameObject GetPooledObject()
+    private T AddObjectToPool()
     {
-        for (var i = 0; i < AmountToPool; i++)
-        {
-            if (!pooledObjects[i].activeInHierarchy)
-            {
-                return pooledObjects[i];
-            }
-        }
-        return null;
+        var tmp = Instantiate(
+            objectToPool,
+            PoolContainer.transform,
+            true
+            );
+        tmp.name = $"{objectToPool.name}-{_count++}";
+        tmp.gameObject.SetActive(false);
+        pooledObjects.Add(tmp);
+        return tmp;
     }
-    
-    public void ReturnToPool(GameObject obj)
+
+    public T GetPooledObject() {
+        if (pooledObjects is null)
+            return null;
+        
+        foreach (var t in pooledObjects.Where(t => !t.gameObject.activeInHierarchy))
+        {
+            return t;
+        }
+
+        // If no inactive object is found, add a new one to the pool and return it
+        return AddObjectToPool();
+    }
+
+    public void ReturnToPool(T objectToReturn)
     {
-        obj.SetActive(false);
+        objectToReturn.gameObject.SetActive(false);
     }
 }
