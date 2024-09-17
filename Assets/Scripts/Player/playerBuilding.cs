769e1305f905eeaacf469e47f3261788d5275e93
AdamBougher <a.bougher1999@gmail.com>
2024-09-16 23:07:11 -0500

    flushed out building system

--- Assets/Scripts/Player/playerBuilding.cs
+++ Assets/Scripts/Player/playerBuilding.cs
@@ -1,19 +1,20 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
+using NavMeshPlus.Components;
 using UnityEngine.Tilemaps;
 
 public class playerBuilding : MonoBehaviour {
-    public bool buildModeEnabled = false;
     public Grid grid; // Reference to the Grid object
     public Sprite Tool;
     public RuleTile fenceTile;
     public GameObject turret;
+    public AudioClip buildSound;
     
-    public int currentItem = 0;
-
+    
     public SpriteRenderer currentPlacement;
     private Camera _mainCamera;
+    private AudioSource _audioSource;
+    private NavMeshSurface _surface2D;
+    
     public void SetArms() {
         GetComponent<SpriteRenderer>().sprite = Tool;
         currentPlacement.sprite = fenceTile.m_DefaultSprite;
@@ -21,37 +22,29 @@ public class playerBuilding : MonoBehaviour {
 
     private void Start() {
         _mainCamera = Camera.main;
+        _audioSource = GetComponentInParent<AudioSource>();
+        _surface2D = FindObjectOfType<NavMeshSurface>();
     }
 
     private void Update() {
-        if (!buildModeEnabled) {
-            if (currentPlacement != null && currentPlacement.gameObject.activeSelf) {
-                currentPlacement.gameObject.SetActive(false);
-            }
-            return;
-        }
 
-        if (currentPlacement != null && !currentPlacement.gameObject.activeSelf) {
-            currentPlacement.gameObject.SetActive(true);
-        }
-
-        Vector3 mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        var mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = 0f; // Ensure the z position is 0
 
         // Snap the mouse position to the nearest grid point
-        Vector3Int cellPosition = grid.WorldToCell(mousePosition);
-        Vector3 snappedPosition = grid.GetCellCenterWorld(cellPosition);
+        var cellPosition = grid.WorldToCell(mousePosition);
+        var snappedPosition = grid.GetCellCenterWorld(cellPosition);
 
         currentPlacement.transform.position = snappedPosition;
 
         // Snap the rotation to the nearest 90 degrees
-        float angle = Mathf.Round(currentPlacement.transform.rotation.eulerAngles.z / 90f) * 90f;
+        var angle = Mathf.Round(currentPlacement.transform.rotation.eulerAngles.z / 90f) * 90f;
         currentPlacement.transform.rotation = Quaternion.Euler(0, 0, angle);
-    }
-    
-    private void OnFire(InputAction.CallbackContext context){
-        if (!buildModeEnabled) return;
         
+    }
+
+    public void Place() {
+        Debug.Log("Build!");
         var mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = 0f; // Ensure the z position is 0
 
@@ -62,10 +55,7 @@ public class playerBuilding : MonoBehaviour {
         // Create a new tile at the snapped position
         var tilemap = grid.GetComponentInChildren<Tilemap>();
         tilemap.SetTile(cellPosition, fenceTile);
+        _audioSource.PlaySound(buildSound);
+        _surface2D.UpdateNavMesh(_surface2D.navMeshData);
     }
-    
-    private void OnSwapBuild(InputValue value) {
-        Debug.Log(value.Get<float>() + "Was pressed");
-    }
-    
 }
